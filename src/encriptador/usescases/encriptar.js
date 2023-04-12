51e9e7d1b9a889b53b6b2aa2471ec4a2cbb91e18
javier <javiro76@gmail.com>
2023-04-12 18:47:18 -0500

    se completa funcion de encriptacion

--- src/encriptador/usescases/encriptar.js
+++ src/encriptador/usescases/encriptar.js
@@ -1,10 +1,30 @@
 
-let texto = 'Hola mundo todas e';
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+let enter = 'enter',
+    imes  = 'imes', 
+    ai   = 'ai',
+    ober = 'ober',
+    ufat = 'ufat';
+
+
+
+let texto = 'gato';
 
 
 const encriptar = (texto) => {
 
-      const nuevaCandena = texto.replaceAll('a', '4');
+      const nuevaCandena = texto.replaceAll('e', enter).
+                                 replaceAll('i', imes).
+                                 replaceAll('a', ai).
+                                 replaceAll('o', ober).
+                                 replaceAll('u', ufat);
       console.log(nuevaCandena);
       console.log(typeof(nuevaCandena));
 
