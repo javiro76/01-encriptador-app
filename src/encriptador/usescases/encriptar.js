518f27e883ee72376fa5c976105bdbc109c85bbd
javier <javiro76@gmail.com>
2023-04-12 15:02:38 -0500

    se realiza funcion encriptar

--- src/encriptador/usescases/encriptar.js
+++ src/encriptador/usescases/encriptar.js
@@ -1,2 +1,15 @@
 
+let texto = 'Hola mundo todas e';
 
+
+const encriptar = (texto) => {
+
+      const nuevaCandena = texto.replaceAll('a', '4');
+      console.log(nuevaCandena);
+      console.log(typeof(nuevaCandena));
+
+}
+
+
+//console.log('hola mundo');
+encriptar(texto);
