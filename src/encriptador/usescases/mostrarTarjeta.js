b84cb98e067958d3a6fbd437c58ab78c811a6086
javier <javiro76@gmail.com>
2023-04-18 13:49:21 -0500

    se inicializa funcion mostrarTarjeta

---
+++ src/encriptador/usescases/mostrarTarjeta.js
@@ -0,0 +1,12 @@
+
+
+
+export const mostrarTarjeta = ( element, element2 ) => {
+
+    element = document.getElementById(element).style.display = "block";
+    element2 = document.getElementById(element2).style.display = "none";
+
+};
+
+
+
